1974269efe4a1889073db66f14ddb6e00b5ac28c
Julio Rodrigues <juulhao@MacBook-Pro-de-Julio.local>
2022-10-22 02:25:07 -0300

    ajustando rotas

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RotuloController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login.api');
     Route::post('/register', [AuthController::class, 'register'])->name('register.api');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout.api');
-    Route::get('/rotulos', [RotuloController::class, 'getRotulos']);
-    Route::get('/pdf', [RotuloController::class, 'getPDF']);
+    Route::post('/rotulos', [RotuloController::class, 'getRotulos']);
+    Route::post('/pdf', [RotuloController::class, 'getPDF']);
 });
