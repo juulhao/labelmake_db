6824683ac40c440378cbd200f7582aac0973998a
Julio Rodrigues <juulhao@MacBook-Pro-de-Julio.local>
2022-10-25 17:22:02 -0300

    health check endpoint

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\RotuloController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['cors', 'json.response']], function () {
+    Route::get('/health-check', function () {
+        return response('Is alive');
+    });
     Route::post('/login', [AuthController::class, 'login'])->name('login.api');
     Route::post('/register', [AuthController::class, 'register'])->name('register.api');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout.api');
